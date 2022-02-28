3f25ffdcaaf588cc2d7a3b97d31f0af2ca32a2ef
Yeewon <yes3427@kookmin.ac.kr>
2022-03-01 01:16:53 +0900

    ✨ feat: mui theme 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./styles/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
