be6b815cc2b4ea0574564df0b5b06cf67d20bae7
Yeewon <yes3427@kookmin.ac.kr>
2022-03-02 00:33:57 +0900

    ♻️ refactor: FilterButtonList에 SearchModal 추가

--- src/components/FIlterButtonList.tsx
+++ src/components/FIlterButtonList.tsx
@@ -1,7 +1,24 @@
-import { Box } from "@mui/material";
-import Button from "../components/Button";
+import { Box, Modal, Typography } from "@mui/material";
+import { useState } from "react";
+import styled from "@emotion/styled";
+import SearchModal from "./SearchModal";
+
+const Button = styled.button`
+  height: 36px;
+  min-width: 64px;
+  margin-left: 10px;
+  padding: 0 16px;
+  border-radius: 4px;
+  outline: 0;
+  border-style: none;
+  cursor: pointer;
+`;
 
 const FilterButtonList = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Box
       sx={{
@@ -14,7 +31,10 @@ const FilterButtonList = () => {
       <Button>👁️ 볼 영상</Button>
       <Button>✅ 본 영상</Button>
       <Button>👍🏻 좋아요 한 영상</Button>
-      <Button>🔍 동영상 검색</Button>
+      <Button onClick={handleOpen}>🔍 동영상 검색</Button>
+      <Modal open={open} onClose={handleClose}>
+        <SearchModal />
+      </Modal>
     </Box>
   );
 };
