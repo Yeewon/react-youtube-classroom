6967063b8dfd4d92179ec762d52c1a9574e81f42
Yeewon <yes3427@kookmin.ac.kr>
2022-03-02 17:24:06 +0900

    ✨ feat: useLocalStorage 훅 구현

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+type ReturnType<T> = [T, React.Dispatch<React.SetStateAction<T>>];
+
+const useLocalStorage = <T,>(key: string, initialValue: T): ReturnType<T> => {
+  const [state, setState] = useState<T>(() => {
+    if (!initialValue) return;
+    try {
+      const value = localStorage.getItem(key);
+      return value ? JSON.parse(value) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    if (state) {
+      try {
+        localStorage.setItem(key, JSON.stringify(state));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [state, key]);
+  return [state, setState];
+};
+
+export default useLocalStorage;
