5e2a418fc1e48a0a2c1c19924d488d14f99158a6
Yeewon <yes3427@kookmin.ac.kr>
2022-03-02 17:27:54 +0900

    ✨ feat:  최근 검색 키워드 저장 기능 구현

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -4,17 +4,47 @@ import SearchForm from "./SearchForm";
 import LatestKeywordList from "./LatestKeywordList";
 import SavedVideoCountSection from "./SavedVideoCountSection";
 import VideoSearchResultList from "./VideoSearchResultList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Video } from "../models/Video";
+import { getSearchResult } from "../api/getSearchResult";
+import { formatVideo } from "../utils/video";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { MAX_STORE_KEYWORD_COUNT } from "../constants/classroom";
 
 const SearchModal = () => {
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Video[]>([]);
+  const [savedVideoCount, setSavedVideoCount] = useState(0);
+  const [latestKeywordList, setLatestKeywordList] = useLocalStorage<string[]>(
+    "latestKeywordList",
+    [],
+  );
+
+  const handleSubmit = async (keyword: string) => {
+    const { items } = await getSearchResult(keyword);
+    const formattedResults = formatVideo(items);
+    setResults(formattedResults);
+    updateLatestKeywordList(keyword);
+  };
+
+  const updateLatestKeywordList = (newKeyword: string) => {
+    let newKeywordList = [...latestKeywordList];
+    const targetIndex = newKeywordList.indexOf(newKeyword);
+
+    targetIndex > -1 && newKeywordList.splice(targetIndex, 1);
+    newKeywordList =
+      newKeywordList.length >= MAX_STORE_KEYWORD_COUNT
+        ? newKeywordList.slice(0, MAX_STORE_KEYWORD_COUNT - 1)
+        : newKeywordList;
+
+    setLatestKeywordList([newKeyword, ...newKeywordList]);
+  };
 
   return (
     <Box sx={style}>
       <Title>🔎 유튜브 검색</Title>
-      <SearchForm />
-      <LatestKeywordList keywordList={["여름", "겨울", "가을"]} />
-      <SavedVideoCountSection savedVideoCount={5} />
+      <SearchForm onSubmit={handleSubmit} />
+      <LatestKeywordList keywordList={latestKeywordList} />
+      <SavedVideoCountSection savedVideoCount={savedVideoCount} />
       <VideoSearchResultList searchResultList={results} />
     </Box>
   );
