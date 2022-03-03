97f6a1c921d7810a2415d2dfaf789b7b8478e364
Yeewon <yes3427@kookmin.ac.kr>
2022-03-04 00:28:00 +0900

    ✨ feat: 영상 필터링 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FilterButtonList from "./components/FIlterButtonList";
 import SavedVideoList from "./components/SavedVideoList";
 import Title from "./components/Title";
@@ -7,8 +7,35 @@ import useLocalStorage from "./hooks/useLocalStorage";
 import { Video } from "./models/Video";
 
 const App = () => {
-  const [initialState] = useLocalStorage<Video[]>("savedVideoList", []);
-  const [videoList, setVideoList] = useState<Video[]>(initialState);
+  const [displayOption, setDisplayOption] = useState("toWatch");
+  const [videoList, setVideoList] = useLocalStorage<Video[]>(
+    "savedVideoList",
+    [],
+  );
+  const [displayVideoList, setDisplayVideoList] = useState(videoList);
+
+  useEffect(() => {
+    filterVideo(displayOption);
+  }, [videoList]);
+
+  const filterVideo = (option: string) => {
+    let newVideoList: Video[] = [];
+    if (option === "toWatch") {
+      newVideoList = videoList.filter(({ status }) => !status.isWatched);
+    }
+    if (option === "watched") {
+      newVideoList = videoList.filter(({ status }) => status.isWatched);
+    }
+    if (option === "liked") {
+      newVideoList = videoList.filter(({ status }) => status.isLiked);
+    }
+    setDisplayVideoList(newVideoList);
+  };
+
+  const handleDisplay = (option: string) => {
+    filterVideo(option);
+    setDisplayOption(option);
+  };
 
   const onSaveVideo = (savedVideoList: Video[]) => {
     setVideoList(savedVideoList);
@@ -28,8 +55,15 @@ const App = () => {
       }}
     >
       <Title>👩🏻‍💻 예원의 유튜브 강의실 👨🏻‍💻</Title>
-      <FilterButtonList onSaveVideo={onSaveVideo} />
-      <SavedVideoList videoList={videoList} onClick={handleClickStatusButton} />
+      <FilterButtonList
+        display={displayOption}
+        onSaveVideo={onSaveVideo}
+        onDisplay={handleDisplay}
+      />
+      <SavedVideoList
+        videoList={displayVideoList}
+        onClick={handleClickStatusButton}
+      />
     </Box>
   );
 };
