37769d4e3d9a90b4ca233337221b02f3ca0f963b
Yeewon <yes3427@kookmin.ac.kr>
2022-03-06 15:41:50 +0900

    ♻️ refactor: EMPTY_VIDEO_MSG 수정

--- src/constants/classroom.ts
+++ src/constants/classroom.ts
@@ -2,4 +2,4 @@ export const FETCH_VIDEO_COUNT = 10;
 export const MAX_STORE_KEYWORD_COUNT = 3;
 export const MAX_SAVED_VIDEO_COUNT = 100;
 
-export const EMPTY_VIDEO_MSG = `영상이 없습니다. 😥`;
+export const EMPTY_VIDEO_MSG = "영상이 없습니다.";
