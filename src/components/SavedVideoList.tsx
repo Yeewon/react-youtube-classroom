b41bfb4db6c2e74155dc5e2700bec0f308a676c8
Yeewon <yes3427@kookmin.ac.kr>
2022-03-06 15:43:15 +0900

    ✨ feat: 영상 없는 경우 로티 보여주는 기능 구현

--- src/components/SavedVideoList.tsx
+++ src/components/SavedVideoList.tsx
@@ -1,7 +1,8 @@
-import { Box, Snackbar } from "@mui/material";
+import { Box, Snackbar, Typography } from "@mui/material";
 import { useState } from "react";
 import { EMPTY_VIDEO_MSG } from "../constants/classroom";
 import { Video } from "../models/Video";
+import LottieIcon from "./lottieIcon";
 import SavedVideoArticle from "./SavedVideoArticle";
 
 type Props = {
@@ -37,19 +38,35 @@ const SavedVideoList = ({ videoList, onClick, onDelete }: Props) => {
         justifyContent: "center",
       }}
     >
-      {videoList.length
-        ? videoList.map((aVideo, index) => {
-            return (
-              <SavedVideoArticle
-                key={index}
-                videoInfo={aVideo}
-                onClick={onClick}
-                onSnackbar={onSnackbar}
-                onDelete={onDelete}
-              />
-            );
-          })
-        : EMPTY_VIDEO_MSG}
+      {videoList.length ? (
+        videoList.map((aVideo, index) => {
+          return (
+            <SavedVideoArticle
+              key={index}
+              videoInfo={aVideo}
+              onClick={onClick}
+              onSnackbar={onSnackbar}
+              onDelete={onDelete}
+            />
+          );
+        })
+      ) : (
+        <Box
+          sx={{
+            mt: 10,
+          }}
+        >
+          <LottieIcon type={"noVideo"} />
+          <Typography
+            align="center"
+            sx={{
+              mt: 5,
+            }}
+          >
+            {EMPTY_VIDEO_MSG}
+          </Typography>
+        </Box>
+      )}
       <Snackbar
         open={open}
         autoHideDuration={3000}
