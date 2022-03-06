eb5ed338a77e2111353badb4f198452b9f61c33a
Yeewon <yes3427@kookmin.ac.kr>
2022-03-06 15:40:39 +0900

    ✨ feat: lottieIcon 컴포넌트 구현

---
+++ src/components/lottieIcon.tsx
@@ -0,0 +1,51 @@
+import { useRef, useEffect } from "react";
+import lottie from "lottie-web";
+import noVideo from "../assets/no-video-lottie.json";
+import { Box } from "@mui/material";
+
+type Props = {
+  type: string;
+};
+
+type lottieType = {
+  [key: string]: {
+    lottie: object;
+    loop: boolean;
+  };
+};
+
+const lottieTypes: lottieType = {
+  noVideo: {
+    lottie: noVideo,
+    loop: true,
+  },
+};
+
+const LottieIcon = ({ type }: Props) => {
+  const lottieIcon = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (lottieIcon.current) {
+      lottie.loadAnimation({
+        container: lottieIcon.current,
+        renderer: "svg",
+        loop: lottieTypes[type].loop,
+        autoplay: true,
+        animationData: lottieTypes[type].lottie,
+      });
+    }
+  }, [type]);
+
+  return (
+    <Box
+      sx={{
+        height: 200,
+        mt: "-30px",
+        background: "transparent",
+      }}
+      ref={lottieIcon}
+    />
+  );
+};
+
+export default LottieIcon;
