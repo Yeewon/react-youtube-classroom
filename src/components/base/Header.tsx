0325c5ea3ca2b4e7248fbe3aa1796fa93ce5eda6
Yeewon <yes3427@kookmin.ac.kr>
2022-03-07 21:24:56 +0900

    ♻️ refactor: Header 컴포넌트 분리

---
+++ src/components/base/Header.tsx
@@ -0,0 +1,16 @@
+import Title from "@/components/base/Title";
+import { Box } from "@mui/system";
+
+const Header = () => {
+  return (
+    <Box
+      sx={{
+        mt: 5,
+      }}
+    >
+      <Title>👩🏻‍💻 예원의 유튜브 강의실 👨🏻‍💻</Title>
+    </Box>
+  );
+};
+
+export default Header;
