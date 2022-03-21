f09236a578dc07795abb663843f15286a44713e5
Yeewon <yes3427@kookmin.ac.kr>
2022-03-22 01:46:14 +0900

    ✨ feat: cypress 테스트 코드 작성

---
+++ cypress/integration/classroom.spec.tsx
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+// @ts-check
+
+describe("나만의 유튜브 강의실", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("cypress test!", () => {});
+});
