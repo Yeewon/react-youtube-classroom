a85f7b90ffeba3f3066a8c01f3cd06954b1f802d
Yeewon <yes3427@kookmin.ac.kr>
2022-03-22 20:23:41 +0900

    ✨ feat: 동영상 검색 테스트 코드 작성

--- cypress/integration/classroom.spec.tsx
+++ cypress/integration/classroom.spec.tsx
@@ -1,10 +1,27 @@
 /// <reference types="cypress" />
 // @ts-check
+/* eslint-disable jest/valid-expect */
+
+import { FETCH_VIDEO_COUNT } from "../../src/constants/classroom";
 
 describe("나만의 유튜브 강의실", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+  const keyword = "TypeScript";
+
+  it("successfully loads", () => {
+    cy.exec("npm start");
+    cy.visit("/");
   });
 
-  it("cypress test!", () => {});
+  it("동영상을 검색하면 결과를 사용자에게 보여준다.", () => {
+    cy.searchVideo(keyword);
+    cy.get('button[type="submit"]')
+      .click()
+      .then(() => {
+        cy.get("iframe").should("have.length", FETCH_VIDEO_COUNT);
+      });
+  });
+
+  it("최근 검색 키워드 버튼 생성을 생성한다.", () => {
+    cy.get("#keyword-button").should("have.text", keyword);
+  });
 });
